e3f30114676c468d4288ca624620dc5722fd4da0
Anton <antonrual@gmail.com>
2018-12-03 17:17:42 +0300

    Чтение из файла первой работы

--- #1del notsort element.py
+++ #1del notsort element.py
@@ -1,24 +1,32 @@
 # Из некоторого массива необходимо удалить все неотсортированные элементы
 
-mas_elementary = [6, 8, 2, 11, 4, 9, 20, 55]
+#mas_elementary = [6, 8, 2, 11, 4, 9, 20, 55]
+with open('#1delete notsort element.txt', 'r') as f:
+    for mas in f:
+        mas = mas.strip().split(' ')
+        #print(mas)
+mas_elementary = [int(t) for t in mas]
+print('Массив Изначальных элементов:', mas_elementary)
 result_mas = []
 element_iterable, t = 0, len(mas_elementary)-1
 while t > 0:
-  try:
-    if mas_elementary[element_iterable] < mas_elementary[element_iterable+1]: #68,811,1120,2055,
-      if mas_elementary[element_iterable] not in result_mas:
-          result_mas.append(mas_elementary[element_iterable])
-      element_iterable+=1
-    elif mas_elementary[element_iterable] > mas_elementary[element_iterable+1]: #82-2иск, 114-4иск,119-9иск
-        if mas_elementary[element_iterable] not in result_mas:
-          result_mas.append(mas_elementary[element_iterable])
-        mas_elementary.pop(element_iterable+1)
-        continue
-    t-=1
-  except IndexError:
-    print("Достигнут последний элемент массива:")
-    print("Массив mas_elementary преобразован в", mas_elementary)
-    if mas_elementary[-1] > result_mas[-1]:
-      result_mas.append(mas_elementary[-1])
-      print("Результирующий массив", result_mas)
-    break
+    try:
+        if mas_elementary[element_iterable] < mas_elementary[element_iterable+1]: #68,811,1120,2055,
+            if mas_elementary[element_iterable] not in result_mas:
+                result_mas.append(mas_elementary[element_iterable])
+            element_iterable+=1
+        elif mas_elementary[element_iterable] > mas_elementary[element_iterable+1]: #82-2иск, 114-4иск,119-9иск
+            if mas_elementary[element_iterable] not in result_mas:
+                result_mas.append(mas_elementary[element_iterable])
+            mas_elementary.pop(element_iterable+1)
+            continue
+        t-=1
+    except IndexError:
+        print("Достигнут последний элемент массива:")
+        print("Массив mas_elementary преобразован в", mas_elementary)
+        if mas_elementary[-1] > result_mas[-1]:
+            result_mas.append(mas_elementary[-1])
+            print("Результирующий массив", result_mas)
+        break
+
+
